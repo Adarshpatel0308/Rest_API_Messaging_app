dfbd70e859aef0acd8471ff54c64f1203643f8e9
Adarshpatel0308 <adarshpatel88899@gmail.com>
2025-02-22 00:06:37 +0530

    [Adarsh Patel] add. Create RestCall by passing the name as path variable

--- rest_api/src/main/java/com/demonstrate_rest_api/rest_api/controller/RestControllerAPI.java
+++ rest_api/src/main/java/com/demonstrate_rest_api/rest_api/controller/RestControllerAPI.java
@@ -1,10 +1,7 @@
 package com.demonstrate_rest_api.rest_api.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/hello")
@@ -17,10 +14,16 @@ class RestControllerAPI {
 
     @GetMapping("/UC2")
     @ResponseBody
-    public String sayHello(@RequestParam(value = "name", defaultValue = "Mark") String name) {
+    public String sayHelloByPassName(@RequestParam(value = "name", defaultValue = "Mark") String name) {
         return "Hello " + name + " from BridgeLabz";
 
-        //http://localhost:8080/hello/UC2?name=adarsh --> Browse this to run 
+        //http://localhost:8080/hello/UC2?name=adarsh --> Browse this to run
     }
 
+    @GetMapping("UC3/{name}")
+    public String sayHelloByPathVariable(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+
+        //http://localhost:8080/hello/UC3/adarsh
+    }
 }
